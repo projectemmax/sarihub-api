9c9ffece002aed0e3e82275d1ec1cd024a6ac561
projectemmax <project.emmax@gmail.com>
2026-07-23 17:27:50 +0800

    feat(logger): customize HTTP request logging

--- src/common/logger/logger.config.ts
+++ src/common/logger/logger.config.ts
@@ -2,18 +2,48 @@ import { Params } from 'nestjs-pino';
 
 export const loggerConfig: Params = {
     pinoHttp: {
+        level: process.env.NODE_ENV === 'production'
+            ? 'info'
+            : 'debug',
+
         transport:
-        process.env.NODE_ENV === 'production'
-            ? undefined
-            : {
-                target: 'pino-pretty',
-                options: {
-                    singleLine: true,
+            process.env.NODE_ENV === 'production'
+                ? undefined
+                : {
+                    target: 'pino-pretty',
+                    options: {
                     colorize: true,
                     translateTime: 'SYS:standard',
+                    singleLine: true,
+                    },
                 },
-            },
 
-        level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+
+        redact: {
+            paths: [
+                'req.headers.authorization',
+                'req.headers.cookie',
+                'req.headers["set-cookie"]',
+            ],
+            remove: true,
+        },
+
+        serializers: {
+            req(req) {
+                return {
+                    id: req.id,
+                    method: req.method,
+                    url: req.url,
+                    ip: req.remoteAddress,
+                    userAgent: req.headers['user-agent'],
+                };
+            },
+            res(res) {
+                return {
+                    statusCode: res.statusCode,
+                };
+            },
+        },
+
     },
 };
