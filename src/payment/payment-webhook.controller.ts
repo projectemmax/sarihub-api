962f08167dc41e159cb00d24fef29de7cbd8a258
projectemmax <project.emmax@gmail.com>
2026-07-23 23:11:50 +0800

    (Fix): Remove logs cloudinary keys  (#11)
    * chore: remove remaining PayMongo debugging logs
    
    * feat(observability): implement logging foundation with request correlation IDs (#8)
    
    * feat(logger): setup production-grade logging foundation
    
    * feat(logger): setup production-grade logging foundation
    
    * feat(logger): setup production-grade Pino logging
    
    * feat(logger): customize HTTP request logging
    
    * feat(logger): add request correlation IDs
    
    * feat(logger): add request correlation IDs
    
    * chore(release): bump version to 0.7.0
    
    * fix: clean all logs (#10)
    
    * Remove logs of Cloudinary keys

--- src/payment/payment-webhook.controller.ts
+++ src/payment/payment-webhook.controller.ts
@@ -27,8 +27,6 @@ export class PaymentWebhookController {
                 ? req.body.toString('utf8')
                 : JSON.stringify(req.body);
 
-        console.log('PayMongo webhook received', rawBody);
-
         const secret = this.configService.get<string>('PAYMONGO_WEBHOOK_SECRET');
 
         if (!secret) {
@@ -50,18 +48,11 @@ export class PaymentWebhookController {
         const eventType = payload.data.attributes.type;
         const checkoutSessionId = payload.data.attributes.data.id;
 
-        console.log('checkoutSessionId:', checkoutSessionId);
-
-        console.log('eventType:', payload.data.attributes.type);
-       
-
         if (eventType === 'checkout_session.payment.paid') {
             await this.paymentService.markPaymentPaid(checkoutSessionId, payload);
-            console.log('Payment successfully marked as PAID');
         }
 
         if (eventType === 'payment.failed') {
-            console.log('Updating payment as FAILED', checkoutSessionId);
             await this.paymentService.markPaymentFailed(checkoutSessionId, payload);
         }
 
